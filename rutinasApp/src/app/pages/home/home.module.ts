db7fd2e9da0c4ae84464c6d3b2484ce9eb3ec0ba
Alberto.Kyu07 <jose.cruzcortes@cesunbc.edu.mx>
2022-08-04 22:50:30 -0700

    Componentes

--- rutinasApp/src/app/pages/home/home.module.ts
+++ rutinasApp/src/app/pages/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { ListadoRutinasComponent } from '../../components/listado-rutinas/listado-rutinas.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, ListadoRutinasComponent],
+  entryComponents: [ListadoRutinasComponent]
 })
 export class HomePageModule {}
