f9052cf858dfc0dab06f5a0fe76f6597c401862b
Alberto.Kyu07 <jose.cruzcortes@cesunbc.edu.mx>
2022-08-08 19:15:38 -0700

    ajustes

--- rutinasApp/src/app/pages/home/home.page.ts
+++ rutinasApp/src/app/pages/home/home.page.ts
@@ -24,15 +24,15 @@ export class HomePage implements OnInit{
 
   categorias = [
     {
-      imagen: 'https://images.unsplash.com/photo-1507398941214-572c25f4b1dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
+      imagen: 'https://img.freepik.com/fotos-premium/torso-musculoso-masculino_144962-3859.jpg?w=2000',
       nombreCategoria:'Tronco'
     },
     {
-      imagen: 'https://images.unsplash.com/photo-1546483875-ad9014c88eba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60',
+      imagen: 'https://previews.123rf.com/images/antondotsenko/antondotsenko1711/antondotsenko171100301/89208810-guapo-musculoso-hombre-cauc%C3%A1sico-de-apariencia-modelo-trabajando-brazos-de-entrenamiento-en-el-gimna.jpg',
       nombreCategoria:'Extremidad Superior'
     },
     {
-      imagen: 'https://images.unsplash.com/photo-1549060279-7e168fcee0c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60',
+      imagen: 'https://st3.depositphotos.com/6944362/17233/i/600/depositphotos_172332518-stock-photo-close-up-of-bodybuilders-muscular.jpg',
       nombreCategoria:'Extremidad Inferior'
     }
   ]
